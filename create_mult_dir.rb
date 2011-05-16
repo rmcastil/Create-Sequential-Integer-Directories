6d1bd3f02c31cb5b4cf76360ad6edd7a3c3926a9
Ryan Castillo <rmcastil@gmail.com>
2011-05-16 19:19:47 -0400

    Added source to actually create the directories

--- create_mult_dir.rb
+++ create_mult_dir.rb
@@ -1,14 +1,12 @@
 #!/usr/bin/ruby
 class DirectoryName
-   def initialize
-   end
 
    def to_s
       Dir.getwd
    end
 
    def create_dir(dir_name)
-      Dir.mkdir(dir_name)
+      Dir.mkdir(dir_name) 
    end
 
    def create_sequence_array_to(size)
@@ -16,14 +14,7 @@ class DirectoryName
       1.upto(size) {|i| a.push(i)}
       return a
    end
-# begin
-#      directory_name = Dir::pwd + "/" + "hdfr"#tickler_files"
-#      puts directory_name
-#      puts FileTest::directory?(directory_name)
-#      if FileTest::directory?(directory_name)
-#         return
-#   end
-#      puts "always get here?"
-      #Dir::mkdir(directory_name)
-#   end
 end
+
+foo = DirectoryName.new()
+foo.create_sequence_array_to(2).each {|i| foo.create_dir(i.to_s())}
